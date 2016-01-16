ffe522365e79103a8d046128e1376e3db4a11476
Alexoner <onerhao@gmail.com>
2016-01-16 18:18:40 +0800

    restore shadowsocks

--- lib/restore.sh
+++ lib/restore.sh
@@ -7,7 +7,7 @@ APP_PATH="$HOME/.synccf"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew tap caskroom/cask
-brew install python, python3
+brew install python, python3, node
 # brew install git
 
 # download synccf repository
@@ -20,6 +20,15 @@ brew install neovim/neovim/neovim
 
 #install my vim conf
 curl https://raw.githubusercontent.com/Alexoner/spf13-vim/3.0/bootstrap.sh -L > spf13-vim.sh && sh spf13-vim.sh
+
+#install shadowsocks-heroku
+SOFT_DIR="$HOME/Documents/soft"
+mkdir "$SOFT_DIR"
+git clone https://github.com/mrluanma/shadowsocks-heroku.git "$SOFT_DIR"
+cd "$SOFT_DIR/shadowsocks-heroku" && npm install
+echo '#!/bin/sh' > run.sh
+echo 'node local.js -s xxx-island.herokuapp.com -l 1081 -m rc4 -k xxx11yy -r 80 1>> ss.log  2> ss.log' >> run.sh
+chmod +x run.sh
 
 #install brew packages
 "$BASEDIR../data/homebrew/restore-homebrew.sh" &
