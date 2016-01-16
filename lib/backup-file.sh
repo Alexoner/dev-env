a5093094f13465325b30e18cf8bef37058f9217c
Alexoner <onerhao@gmail.com>
2016-01-16 17:40:32 +0800

    rsync a list of patterns

--- lib/backup-file.sh
+++ lib/backup-file.sh
@@ -2,5 +2,5 @@
 
 BASEDIR=$(dirname "$0")
 rsync -av /etc/hosts "$BASEDIR/../data/etc/"
-rsync -av --files-from="$BASEDIR/../conf/home-files.conf" "$HOME" "$BASEDIR/../data/home"
-rsync -av "$HOME/Library/Fonts" "$BASEDIR/../data/home/.fonts"
+rsync -avz --include-from "$BASEDIR/../conf/home-files.lst" --exclude='*' "$HOME/" "$BASEDIR/../data/home"
+# rsync -av "$HOME/Library/Fonts" "$BASEDIR/../data/home/.fonts"
