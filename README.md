553b749659d1a23564a8bab7397bc010d2b3de8a
Alexoner <onerhao@gmail.com>
2016-01-16 18:10:05 +0800

    restoring script

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # sync-sys
 Synchronize my my operating system
+
+## Backup
+1. In the root of the repository
+```shell
+    lib/backup.sh
+```
+2. Now the data would have been put in ./data/
+
+
+## Restore
--- lib/restore.sh
+++ lib/restore.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+BASEDIR=$(dirname "$0")
 APP_PATH="$HOME/.synccf"
 
 # install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew tap caskroom/cask
-brew install python, python3, git
+brew install python, python3
+# brew install git
 
 # download synccf repository
 git clone https://github.com/Alexoner/synccf.git "$APP_PATH"
@@ -20,10 +22,24 @@ brew install neovim/neovim/neovim
 curl https://raw.githubusercontent.com/Alexoner/spf13-vim/3.0/bootstrap.sh -L > spf13-vim.sh && sh spf13-vim.sh
 
 #install brew packages
-./restore-homebrew.sh
+"$BASEDIR../data/homebrew/restore-homebrew.sh" &
+#install python packages
+python2 -m easy_install pip
+python3 -m easy_install pip
+install_python2_packages() {
+    sudo python2 -m pip install -r "$BASEDIR/../data/python/requirement2.txt" 
+}
+
+install_python3_packages() {
+    sudo python3 -m pip install -r "$BASEDIR/../data/python/requirement3.txt" 
+}
+
+install_python2_packages &
+install_python3_packages &
 
 #################### CREATE SYMBOL LINK ######################
 echo "source $APP_PATH/home/.bashrc" >> "$HOME/.bashrc"
 echo "source $APP_PATH/home/.zshrc" >> "$HOME/.zsshrc"
 
 ln -sv "$APP_PATH/home/*" "$HOME/"
+wait
