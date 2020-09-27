55875f40cb0c3d8a39632b25bfe48abf65ef80b2
Alexoner <onerhao@gmail.com>
2020-09-27 19:14:08 +0800

    cross-platform[linux]: install tmux

--- lib/init-tmux.sh
+++ lib/init-tmux.sh
@@ -8,9 +8,15 @@ setup_tmux() {
 
 	################################### Tmux ############################################
 	export tmux_conf_new_window_retain_current_path=true
+	export tmux_conf_theme_highlight_focused_pane=true
 	#alias tclear="clear && tmux clear" # use Ctrl+L to clear
 	EOF
 
 }
+
+if [ "$(uname -s)" == "Linux" ]
+then
+	sudo apt install -y tmux
+fi
 
 setup_tmux
