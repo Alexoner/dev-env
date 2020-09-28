6b0778d687984ae5f4cb19fa3e0d35e133e85b16
Alexoner <onerhao@gmail.com>
2020-09-28 17:59:09 +0800

    cross-platform[linux]: install linux tools

---
+++ lib/init-ops.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+# install essential tools for debugging, system management
+if [ "$(uname -s)" == "Linux" ]
+then
+	sudo apt install -y strace net-tools gdb nginx iftop iotop \
+		 linux-tools-common linux-tools-generic linux-tools-`uname -r`
+fi
