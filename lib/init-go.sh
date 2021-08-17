162a287eba071e80dbff05228bf964063281c2fa
onerhao <onerhao@gmail.com>
2021-08-17 16:14:24 +0800

    fix username

--- lib/init-go.sh
+++ lib/init-go.sh
@@ -9,10 +9,10 @@ setup_go() {
 	bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 
 	cat << \EOF >> $HOME/.init.sh
-[[ -s "/home/haodu/.gvm/scripts/gvm"  ]] && source "/home/haodu/.gvm/scripts/gvm"
+[[ -s "$HOME/.gvm/scripts/gvm"  ]] && source "$HOME/.gvm/scripts/gvm"
 EOF
 
-	source /home/haodu/.gvm/scripts/gvm
+	source $HOME/.gvm/scripts/gvm
 	# Go 1.5+ removed the C compilers from the toolchain. 
 	# To install go 1.5+, you must have go installed, or install from binary with -B
 	gvm install go1.4 -B
