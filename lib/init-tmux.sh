acccc71e88735a975c8bca28cc0f10ac002c4e3a
H <haoisworking@gmail.com>
2023-09-09 18:37:53 -0700

    Install tmux conf from upstream

--- lib/init-tmux.sh
+++ lib/init-tmux.sh
@@ -11,7 +11,11 @@ setup_tmux() {
 	export tmux_conf_theme_highlight_focused_pane=true
 	#alias tclear="clear && tmux clear" # use Ctrl+L to clear
 	EOF
-
+ 	# install https://github.com/gpakosz/.tmux from upstream
+	cd
+	git clone https://github.com/gpakosz/.tmux.git
+	ln -s -f .tmux/.tmux.conf
+	~/.dev-env/.tmux.conf.local ~/
 }
 
 if [ "$(uname -s)" == "Linux" ]
